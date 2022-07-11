7fc60bb3fdfc8eef5db6f5022b86497e47df9b7c
Nithin <60174062+Nithin3008@users.noreply.github.com>
2022-07-11 11:11:00 +0530

    Added msg if amount is higher

--- app.js
+++ app.js
@@ -1,47 +1,41 @@
-var bill =document.querySelector("#billamount")
-var cash=document.querySelector("#cashgiven")
-var check1=document.querySelector("#checkbutton")
-var noOfnotes=document.querySelectorAll(".no-of-notes")
-var message1=document.querySelector("#msg")
-var remainAmount=0
+var bill = document.querySelector("#billamount")
+var cash = document.querySelector("#cashgiven")
+var check1 = document.querySelector("#checkbutton")
+var noOfnotes = document.querySelectorAll(".no-of-notes")
+var message1 = document.querySelector("#msg")
+var remainAmount = 0
 
-var notes=[2000, 500, 100, 20, 10, 5, 1]
+var notes = [2000, 500, 100, 20, 10, 5, 1]
 
 
-function handler1()
-{
-    let bill1=Number(bill.value);
-    let cash1=Number(cash.value)
-    if(bill1<=0)
-    {
-        message1.innerText="enter valid amount";
+function handler1() {
+    let bill1 = Number(bill.value);
+    let cash1 = Number(cash.value)
+    if (bill1 <= 0) {
+        message1.innerText = "enter valid amount";
 
     }
-  else if(bill1>cash1)
-    {
-        message1.innerText="please enter valid amount"
+    else if (bill1 > cash1) {
+        message1.innerText = "Bill is greater than cash so give valid amount"
     }
-    else if(bill1===cash1)
-    {
-        message1.innerText="No need to give change"
+    else if (bill1 === cash1) {
+        message1.innerText = "No need to give change"
     }
 
 
-    else if(bill1<=cash1)
-    {
-        var amount_returned=cash1-bill1
-        for(var i=0;i<=notes.length;i++)
-        {
-            var result=Math.trunc(amount_returned/notes[i]);
-            
-            amount_returned=amount_returned%notes[i]
+    else if (bill1 <= cash1) {
+        var amount_returned = cash1 - bill1
+        for (var i = 0; i <= notes.length; i++) {
+            var result = Math.trunc(amount_returned / notes[i]);
+
+            amount_returned = amount_returned % notes[i]
             console.log(result)
-            noOfnotes[i].innerText=result
+            noOfnotes[i].innerText = result
         }
     }
 
 }
-check1.addEventListener("click",handler1)
+check1.addEventListener("click", handler1)
 
 
 
