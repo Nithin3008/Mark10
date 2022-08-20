33da4c8003997fd330dfc6a02ac493eb33eb212d
Nithin <60174062+Nithin3008@users.noreply.github.com>
2022-08-20 11:08:17 +0530

    Added new functionality

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ var notes = [2000, 500, 100, 20, 10, 5, 1]
 
 function handler1() {
     let bill1 = Number(bill.value);
-    let cash1 = Number(cash.value)
-    if (bill1 <= 0) {
-        message1.innerText = "enter valid amount";
-
+    let cash1 = Number(cash.value);
+    console.log(bill1, cash1)
+    if (bill1 === 0 || cash1 === 0) {
+        message1.innerText = "Enter the money properly"
     }
+    // if (bill1 <= 0) {
+    //     message1.innerText = "enter valid amount";
+
+    // }
     else if (bill1 > cash1) {
         message1.innerText = "Bill is greater than cash so give valid amount"
     }
